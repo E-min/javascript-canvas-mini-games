8baccd95f1965a1c9007c352a123c9ecb58e5996
Mehmet Emin <89641439+E-min@users.noreply.github.com>
2023-10-29 15:58:11 +0300

    Folder names changed

--- Blueprint/main.js
+++
@@ -1,5 +0,0 @@
-import { gameArea } from "./modules/gameArea.js";
-import { preloadTextures } from "./modules/preLoadTextures.js";
-
-// load textures before starting game
-preloadTextures(() => gameArea.start());
